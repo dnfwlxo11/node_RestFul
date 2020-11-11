e19a5162e4ee1f5eece43b12a427fdda7fc9fe58
Daein <dnfwlxo11@naver.com>
2020-11-12 00:02:28 +0900

    add: get api code but not working

--- route/RESTful.js
+++ route/RESTful.js
@@ -1,37 +1,67 @@
 var express = require('express');
 var mysql = require('mysql');
+var async = require('async');
 var path = require('path');
 var bodyParser = require('body-parser');
-var app = express();
 var dbConfig = require('./mysql_config');
 var router = express.Router();
 
-// body-parser setting, body-parser 설정
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({
-    extended: true
-}));
+router.get('/get', function (req, res) {
+    var cnt = true;
+    var conn = mysql.createConnection(dbConfig);
+    console.log('받은 정보 : ' + req.body.name, req.body.password);
 
-var conn = mysql.createConnection(dbConfig);
+    async.waterfall([
+        function getSQL() {
+            var sql = 'SELECT * FROM node_table where name=?';
+            var query = conn.query(sql, [req.body.name], function (err, rows) {
+                if (err) {
+                    console.log('존재하지 않는 회원이거나 비밀번호가 틀렸습니다. \n다시 한번 확인해 주세요.');
+                    cnt = false;
+                }
 
-router.get('/get', function(req, res) {
-    console.log(req.body);
+                else {
+                    if (rows != '' && rows[0].name == req.body.name && rows[0].pass == req.body.pass) console.log('로그인 후 메인페이지로 이동');
+                    else {
+                        console.log('존재하지 않는 회원이거나 비밀번호가 틀렸습니다. \n다시 한번 확인해 주세요.');
+                        cnt = false;
+                    }
+                }
+
+                conn.end();
+            });
+        },
+    
+        function sendResult() {
+            var data = {
+                'result': cnt
+            };
+        
+            console.log(data, '/get');
+            res.send(data);
+        },
+        function print() {
+            console.log('동기식으로 실행완료')
+        }]
+    );
+
+    
 });
 
-router.post('/post', function(req, res) {
+router.post('/post', function (req, res) {
     console.log('post 실행');
     res.render(path.join(__dirname, '../public/1.ejs'))
 });
 
-router.put('/put', function(req, res) {
+router.put('/put', function (req, res) {
     console.log('put 실행');
 });
 
-router.delete('/delete', function(req, res) {
+router.delete('/delete', function (req, res) {
     console.log('delete 실행');
 });
 
-router.use('/', function(req, res) {
+router.use('/', function (req, res) {
     res.render(path.join(__dirname, '../public/main_loginAfter.ejs'))
 });
 
--- route/pageFunc.js
+++ route/pageFunc.js
@@ -6,34 +6,11 @@ var dbConfig = require('./mysql_config');
 var db = require('./RESTful');
 var path = require('path');
 
-// page move function
-function login(name) {
-    var conn = mysql.createConnection(dbConfig);
-    console.log('받은 정보 : ' + name);
-
-    var sql = 'SELECT * FROM node_table';
-    var query = conn.query(sql, [name], function(err, rows) {
-        if (err) {
-            console.log('존재하지 않는 회원이거나 비밀번호가 틀렸습니다. \n다시 한번 확인해 주세요.');
-            return false;
-        }
-
-        else {
-            console.log('로그인 후 메인페이지로 이동');
-        }
-
-        conn.end();
-    });
-
-    console.log(query);
-    return query.name;
-};
-
 // main
-router.post('/main', function(req, res) {
-    console.log(req.body)
-    if (login(req.body.id)) res.render(path.join(__dirname, '../public/main_loginAfter.ejs'));
-    else res.redirect('/')
+router.post('/main', function (req, res) {
+    console.log(req.body.data, 'asd');
+    if (req.data) res.render(path.join(__dirname, '../public/main_loginAfter.ejs'));
+    else res.redirect('/');
 });
 
 module.exports = router;
