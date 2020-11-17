0504595a3504578d932e9a106aeb872f7edb3054
dnfwlxo11 <dnfwlxo11@naver.com>
2020-11-17 11:07:31 +0900

    add: update, delete api code and is working

--- route/RESTful.js
+++ route/RESTful.js
@@ -7,7 +7,7 @@ var dbConfig = require('./mysql_config');
 const { send } = require('process');
 var router = express.Router();
 
-router.get('/user/:name', function(req, res) {
+router.get('/find/:name', function (req, res) {
     var sess;
     sess = req.session;
     var conn = mysql.createConnection(dbConfig);
@@ -15,12 +15,49 @@ router.get('/user/:name', function(req, res) {
     var data = req.params;
     var sendData = {};
 
-    var query = conn.query(sql, [data.name], function(err, rows) {
+    var query = conn.query(sql, [data.name], function (err, rows) {
+        if (err) {
+            console.log('데이터베이스 에러 발생');
+            sendData.idx = false;
+            sendData.data = 'none';
+            sendData.type = 'error';
+        }
+
+        else {
+            if (rows.length == 0) {
+                console.log('유저 정보 조회실패');
+                sendData.idx = false;
+                sendData.data = 'none';
+                sendData.type = 'error';
+            }
+
+            else {
+                console.log('유저 정보 조회성공');
+                sendData.idx = true;
+                sendData.data = rows[0];
+                sendData.type = 'find_user';
+            }
+        }
+
+        conn.end();
+        res.send(sendData);
+    });
+});
+
+router.get('/user/:name', function (req, res) {
+    var sess;
+    sess = req.session;
+    var conn = mysql.createConnection(dbConfig);
+    var sql = 'SELECT * FROM node_table WHERE name=?';
+    var data = req.params;
+    var sendData = {};
+
+    var query = conn.query(sql, [data.name], function (err, rows) {
         if (err) {
             console.log('데이터베이스 에러 발생');
             sendData.data = false;
         }
-        
+
         else {
             if (rows.length != 0) {
                 console.log('이미 사용중인 아이디입니다.');
@@ -38,7 +75,8 @@ router.get('/user/:name', function(req, res) {
     });
 });
 
-router.get('/user/:name/:pass', function(req, res) {
+
+router.get('/user/:name/:pass', function (req, res) {
     var sess;
     sess = req.session;
     var conn = mysql.createConnection(dbConfig);
@@ -46,7 +84,7 @@ router.get('/user/:name/:pass', function(req, res) {
     var data = req.params;
     var sendData = {};
 
-    var query = conn.query(sql, [data.name], function(err, rows) {
+    var query = conn.query(sql, [data.name], function (err, rows) {
         if (err) {
             console.log('존재하지 않는 회원이거나 비밀번호가 틀렸습니다. \n다시 한번 확인해 주세요.');
             sendData.data = false;
@@ -72,15 +110,60 @@ router.get('/user/:name/:pass', function(req, res) {
     });
 });
 
-router.put('/put', function(req, res) {
+router.delete('/user/:name/:pass', function (req, res) {
+    var conn = mysql.createConnection(dbConfig);
+
+    var sql = 'SELECT * FROM node_table WHERE name=?';
+    var data = req.params;
+    var sendData = {};
+
+    var query = conn.query(sql, [data.name], function (err, rows) {
+        if (err) {
+            console.log('데이터베이스 에러 발생');
+            sendData.data = false;
+            sendData.type = 'error';
+        }
+
+        else {
+            if (rows[0].pass == data.pass) console.log('비밀번호 일치, 삭제 시작');
+
+            else {
+                console.log('비밀번호 불일치')
+                sendData.data = false;
+                sendData.type = 'error';
+            }
+        }
+    });
+
+    var sql = 'DELETE FROM node_table WHERE name=?';
+
+    var query = conn.query(sql, [data.name], function (err, rows) {
+        if (err) {
+            console.log('데이터베이스 에러 발생2');
+            sendData.data = false;
+            sendData.type = 'error';
+        }
+
+        else {
+            console.log('유저 삭제 완료');
+            sendData.data = true;
+            sendData.type = 'delete_user';
+        }
+
+        conn.end();
+        res.send(sendData);
+    });
+});
+
+router.put('/put', function (req, res) {
     console.log('put 실행');
 });
 
-router.patch('/patch', function(req, res) {
+router.patch('/patch', function (req, res) {
     console.log('patch 실행');
 });
 
-router.post('/register/:name/:pass/:memo', function(req, res) {
+router.post('/register/:name/:pass/:memo', function (req, res) {
     var conn = mysql.createConnection(dbConfig);
 
     var sql = 'INSERT INTO node_table(name, pass, memo) VALUES (?, ?, ?)';
@@ -89,7 +172,7 @@ router.post('/register/:name/:pass/:memo', function(req, res) {
 
     if (data.memo.length == 0) data.memo = 'none';
 
-    var query = conn.query(sql, [data.name, data.pass, data.memo], function(err, rows) {
+    var query = conn.query(sql, [data.name, data.pass, data.memo], function (err, rows) {
         if (err) {
             console.log('이미 가입한 회원입니다.');
             sendData.data = false;
@@ -105,8 +188,49 @@ router.post('/register/:name/:pass/:memo', function(req, res) {
     });
 });
 
-router.delete('/delete/:name/:pass', function(req, res) {
-    console.log('delete 실행');
+router.post('/update/:name/:oldpass/:newpass', function (req, res) {
+    var conn = mysql.createConnection(dbConfig);
+    var sql = 'SELECT * FROM node_table WHERE name=?';
+    var data = req.params;
+    var sendData = {};
+
+    var query = conn.query(sql, [data.name], function (err, rows) {
+        if (err) {
+            console.log('데이터베이스 에러 발생');
+            sendData.data = false;
+            sendData.type = 'error';
+        }
+
+        else {
+            if (rows[0].pass == data.oldpass) console.log('비밀번호 일치, 변경 시작');
+
+
+            else {
+                console.log('비밀번호 불일치')
+                sendData.data = false;
+                sendData.type = 'error';
+            }
+        }
+    });
+
+    var sql = 'UPDATE node_table SET pass=? WHERE name=?';
+
+    var query = conn.query(sql, [data.newpass, data.name], function (err, rows) {
+        if (err) {
+            console.log('데이터베이스 에러 발생2');
+            sendData.data = false;
+            sendData.type = 'error';
+        }
+
+        else {
+            console.log('정상적으로 수정 완료, 재로그인 필요');
+            sendData.data = true;
+            sendData.type = 'update_pass';
+        }
+
+        conn.end();
+        res.send(sendData);
+    });
 });
 
 router.use('/', function (req, res) {
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var serverAddr = '127.0.0.1';
 var serverPort = process.env.PORT || 3000;
 
 // start server
-app.listen(serverPort, function(req, res){
+app.listen(serverPort, serverAddr, function(req, res){
     console.log('start Server!!, 서버가 시작되었습니다.');
 });
 
